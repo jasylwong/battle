28c4a16122394b36302c82d73b15a5bcc2d8f98d
Jason <jasylwong@gmail.com>
2020-02-03 22:21:09 +0000

    config.ru file added

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
-set :session_secret, 'super secret'
 
-get '/' do
-    "Hello Battle!"
-end
+class Battle < Sinatra::Base
+    get '/' do
+        "Hello Battle!"
+    end
 
-get '/battle' do
-    "It's battle time everybody!"
+    get '/battle' do
+        "BATTLE TIME!!"
+    end
+    
+    # start the server if ruby file executed directly
+    run! if app_file == $0
 end
